60eef3858645f4d725d3b6983f359b122f40d2c9
Anton <apantev@gmail.com>
2012-07-15 10:19:28 -0400

    Added more shape keys to the vertex shader and made the main code find all the none zero weight shape keys to bind to the shaders.

--- animation.js
+++ animation.js
@@ -5,21 +5,60 @@ function smoothStep(x) {
 function animate(gl, timing) {
 	//Blink
 	if(timing.currentTime < blinkEnd) {
-		var blinkWeight = (blinkEnd-timing.currentTime)/800.0;
-		blinkWeight = 2*smoothStep(blinkWeight);
+		var weight = (blinkEnd-timing.currentTime)/900.0;
+		weight = 2*smoothStep(weight);
 
-		if(blinkWeight < 1.0) {
-			blinkWeight = 1.0 - blinkWeight;
+		if(weight < 1.0) {
+			weight = 1.0 - weight;
 		} else {
-			blinkWeight = blinkWeight - 1.0;
+			weight = weight - 1.0;
 		}
 
 		for(i = 0; i < 4; i++)
-			gl.models["EyeLid.00"+i+".js"].weights[1] = blinkWeight;
+			gl.models["EyeLid.00"+i+".js"].weights[1] = weight;
+	}
+	
+	//Happy
+	if(timing.currentTime < happyEnd) {
+		var weight = (happyEnd-timing.currentTime)/2000.0;
+		weight = 2*smoothStep(weight);
+
+		if(weight > 1.0) {
+			weight = 2.0 - weight;
+		}
+
+		gl.models["Face.js"].weights[6] = weight;
+		gl.models["Face.js"].weights[7] = weight;
+		gl.models["Face.js"].weights[11] = weight;
+		gl.models["Face.js"].weights[12] = weight;
+		gl.models["Face.js"].weights[13] = 0.522*weight;
+	}
+	
+	//Angry
+	if(timing.currentTime < angryEnd) {
+		var weight = (angryEnd-timing.currentTime)/2000.0;
+		weight = 2*smoothStep(weight);
+
+		if(weight > 1.0) {
+			weight = 2.0 - weight;
+		}
+
+		gl.models["Face.js"].weights[4] = weight;
+		gl.models["Face.js"].weights[5] = weight;
+		gl.models["Face.js"].weights[10] = 0.6*weight;
+		gl.models["Face.js"].weights[14] = weight;
 	}
 }
 
 var blinkEnd = new Date().getTime();
 function blinkClicked() {
-	blinkEnd = new Date().getTime()+800;
+	blinkEnd = new Date().getTime()+900;
+}
+var happyEnd = new Date().getTime();
+function happyClicked() {
+	happyEnd = new Date().getTime()+2000;
+}
+var angryEnd = new Date().getTime();
+function angryClicked() {
+	angryEnd = new Date().getTime()+2000;
 }
--- shape_keys.vert
+++ shape_keys.vert
@@ -3,12 +3,21 @@ attribute vec2 aTextureCoord;
 attribute vec3 aPosition0;
 attribute vec3 aPosition1;
 attribute vec3 aPosition2;
+attribute vec3 aPosition3;
+attribute vec3 aPosition4;
+attribute vec3 aPosition5;
 attribute vec3 aNormal0;
 attribute vec3 aNormal1;
 attribute vec3 aNormal2;
+attribute vec3 aNormal3;
+attribute vec3 aNormal4;
+attribute vec3 aNormal5;
 
 uniform float uWeight1;
 uniform float uWeight2;
+uniform float uWeight3;
+uniform float uWeight4;
+uniform float uWeight5;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
@@ -30,6 +39,18 @@ void main(void) {
 	if(uWeight2 != 0.0) {
 		position += uWeight2 * (aPosition2 - aPosition0);
 		normalVec += uWeight2 * (aNormal2 - aNormal0);
+	}
+	if(uWeight3 != 0.0) {
+		position += uWeight3 * (aPosition3 - aPosition0);
+		normalVec += uWeight3 * (aNormal3 - aNormal0);
+	}
+	if(uWeight4 != 0.0) {
+		position += uWeight4 * (aPosition4 - aPosition0);
+		normalVec += uWeight4 * (aNormal4 - aNormal0);
+	}
+	if(uWeight5 != 0.0) {
+		position += uWeight5 * (aPosition5 - aPosition0);
+		normalVec += uWeight5 * (aNormal5 - aNormal0);
 	}
 	
 	gl_Position = uPMatrix * uMVMatrix * vec4(position, 1.0);
